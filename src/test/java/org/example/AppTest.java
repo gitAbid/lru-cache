28919ae7f2917812a3b7f60b73ab8f44d673163b
Abid Hasan <abid.northsouth@gmail.com>
2020-09-04 10:44:26 +0600

    Add multi threaded test cases

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -11,62 +11,114 @@ import static org.junit.jupiter.api.Assertions.assertNull;
  * Unit test for simple App.
  */
 public class AppTest {
-    private LRUCache<Integer, Integer> c;
+    private LRUCache<Integer, Integer> cache;
 
     public AppTest() {
-        this.c = new LRUCache<>(2, 5000, TimeUnit.MILLISECONDS);
+        this.cache = new LRUCache<>(2, 5000, TimeUnit.MILLISECONDS);
     }
 
     @Test
     public void testCacheStartsEmpty() {
-        assertNull(c.get(1));
+        assertNull(cache.get(1));
     }
 
     @Test
     public void testSetBelowCapacity() {
-        c.put(1, 1);
-        assertEquals(c.get(1), 1);
-        assertNull(c.get(2));
-        c.put(2, 4);
-        assertEquals(c.get(1), 1);
-        assertEquals(c.get(2), 4);
+        cache.put(1, 1);
+        assertEquals(cache.get(1), 1);
+        assertNull(cache.get(2));
+        cache.put(2, 4);
+        assertEquals(cache.get(1), 1);
+        assertEquals(cache.get(2), 4);
     }
 
     @Test
     public void testCapacityReachedOldestRemoved() {
-        c.put(1, 1);
-        c.put(2, 4);
-        c.put(3, 9);
-        assertNull(c.get(1));
-        assertEquals(c.get(2), 4);
-        assertEquals(c.get(3), 9);
+        cache.put(1, 1);
+        cache.put(2, 4);
+        cache.put(3, 9);
+        assertNull(cache.get(1));
+        assertEquals(cache.get(2), 4);
+        assertEquals(cache.get(3), 9);
     }
 
     @Test
     public void testGetRenewsEntry() {
-        c.put(1, 1);
-        c.put(2, 4);
-        assertEquals(c.get(1), 1);
-        c.put(3, 9);
-        assertEquals(c.get(1), 1);
-        assertNull(c.get(2));
-        assertEquals(c.get(3), 9);
+        cache.put(1, 1);
+        cache.put(2, 4);
+        assertEquals(cache.get(1), 1);
+        cache.put(3, 9);
+        assertEquals(cache.get(1), 1);
+        assertNull(cache.get(2));
+        assertEquals(cache.get(3), 9);
     }
 
     @Test
     public void testExpiration() {
-        c.put(1, 1);
-        c.put(2, 4);
-        c.put(3, 9);
-        assertNull(c.get(1));
+        cache.put(1, 1);
+        cache.put(2, 4);
+        cache.put(3, 9);
+        assertNull(cache.get(1));
         try {
             Thread.sleep(4000);
-            assertEquals(c.get(2), 4);
+            assertEquals(cache.get(2), 4);
             Thread.sleep(2000);
-            assertNull(c.get(3));
-            assertEquals(c.get(2), 4);
+            assertNull(cache.get(3));
+            assertEquals(cache.get(2), 4);
             Thread.sleep(6000);
-            assertNull(c.get(2));
+            assertNull(cache.get(2));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Test
+    public void testMultiThreadedPut() {
+        cache = new LRUCache<>(2, 5000, TimeUnit.MILLISECONDS);
+        cache.put(1, 1);
+        cache.put(2, 4);
+        Thread t1 = new Thread(() -> {
+            System.out.println("Thread ->[3,9] ->" + Thread.currentThread());
+            cache.put(3, 9);
+        });
+
+        Thread t2 = new Thread(() -> {
+            System.out.println("Thread ->[4,16] ->" + Thread.currentThread());
+            cache.put(4, 16);
+
+        });
+        t2.start();
+        t1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertNull(cache.get(1));
+        assertNull(cache.get(2));
+        assertEquals(cache.get(4), 16);
+        assertEquals(cache.get(3), 9);
+
+    }
+
+    @Test
+    public void testMultiThreadedGet() {
+        cache.put(1, 1);
+        cache.put(2, 4);
+        Thread t1 = new Thread(() -> {
+            System.out.println("Thread ->[2]" + Thread.currentThread() + "Value -> " + cache.get(2));
+        });
+
+        Thread t2 = new Thread(() -> {
+            System.out.println("Thread ->[1]" + Thread.currentThread() + "Value -> " + cache.get(1));
+        });
+        t2.start();
+        t1.start();
+        try {
+            Thread.sleep(100);
+            cache.printCache();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
